3393c7a712ac9268fb241d7c3a3577805e682d82
Ishan <ishan.kulkarni@somaiya.edu>
2022-04-02 18:54:04 +0530

    FEATURE: Model added

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template
+import tensorflow
 
 app = Flask(__name__)
 
 @app.route('/api/colorization')
 def colorize_image():
     try:
+        model = tensorflow.load_model('./models/colorizer.h5')
         return "test", 200
     except:
         return "Error", 500
 
 @app.route('/')
 def render_html():
-    return render_template('./index.html')
+    return render_template('index.html')
 
 
 if __name__=="__main__":
