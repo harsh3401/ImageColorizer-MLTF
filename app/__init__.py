a520a99e64a6d1b5528f974bcaec450fc7433d84
Ishan <ishan.kulkarni@somaiya.edu>
2022-04-13 03:38:05 +0530

    FIX: SOme errors

--- app/__init__.py
+++ app/__init__.py
@@ -23,14 +23,12 @@ def colorize_image():
             Image.open(image).convert('L').resize((160, 160)) # image resizing,
         )
     image = np.stack((image,)*3, axis=-1)
-    global pred_count
     image = np.reshape(image,(1,160,160,3))
     image = image/255.0
     output_image = model.predict(image)
     single_image = np.clip(output_image, 0.0,1.0).reshape(160,160,3)
     plt.imshow(single_image)
     plt.axis('off')
-    pred_count+=1
     plt.savefig(f'{__path__[0]}/static/predictions/prediction.jpg')
     return 'success'
 
